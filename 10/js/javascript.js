1cd146f8bdddb651140fda9443f61550023c062a
Felipe Fernandes <felipeoficial963@gmail.com>
2021-09-14 15:16:44 -0300

    Origin

---
+++ 10/js/javascript.js
@@ -0,0 +1,10 @@
+
+        function clicar(){
+            var area = document.getElementById("area")
+            area.innerText = "CLICOU"
+            
+        }
+        function entrou(){
+            var area = document.getElementById("area")
+            area.style.background = 'blue'
+        }
